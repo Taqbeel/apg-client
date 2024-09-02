3fcb88649a35280831147bcd01023fa67dc990c5
Muhammad Taqbeel <53563490+Taqbeel@users.noreply.github.com>
2024-09-03 00:23:00 +0500

    lint

--- api/shipment.ts
+++ api/shipment.ts
@@ -1,14 +1,14 @@
 const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/shipping`;
 import axios from "axios";
 
-export const getRates = (payload:object) => {
-  return axios.post(`${url}/getRates`, payload)
-}
+export const getRates = (payload: object) => {
+  return axios.post(`${url}/getRates`, payload);
+};
 
-export const purchaseShipment = (payload:object) => {
-  return axios.post(`${url}/purchaseShipment`, payload)
-}
+export const purchaseShipment = (payload: object) => {
+  return axios.post(`${url}/purchaseShipment`, payload);
+};
 
-export const getLabel = (payload:object) => {
-  return axios.post(`${url}/getLabel`, payload)
-}
+export const getLabel = (payload: object) => {
+  return axios.post(`${url}/getLabel`, payload);
+};
